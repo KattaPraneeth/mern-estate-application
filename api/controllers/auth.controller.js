0803798ccfb87022f3dee74a928c0909c16c89ca
Praneeth <kattapraneeth288@gmail.com>
2024-03-24 13:45:42 +0530

    Complete signup page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -6,6 +6,8 @@ export const signup = async (req, res, next) => {
     const newUser = new User({username, email, password: hashedPassword});
     try {
         await newUser.save();
+        // res.setHeader('Access-Control-Allow-Origin', '*');
+        // res.setHeader('Access-Control-Allow-Credentials', 'true');
         res.status(201).json('user created successfully');
     } catch (error) {
         next(error);
